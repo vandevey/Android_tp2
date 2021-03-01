b52d081f71f3fb77ae0a653b9e61d78d97997088
vandevey <yoan.vandevelde.isn@gmail.com>
2021-03-01 13:16:45 +0100

    bugs fix before branch

--- app/src/main/java/fr/gobelins/edu/vandevelde/tp02_yoan_vandevelde/fragments/AddNeighbourFragment.kt
+++ app/src/main/java/fr/gobelins/edu/vandevelde/tp02_yoan_vandevelde/fragments/AddNeighbourFragment.kt
@@ -6,6 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.basgeekball.awesomevalidation.AwesomeValidation
+import com.basgeekball.awesomevalidation.ValidationStyle
+import com.basgeekball.awesomevalidation.utility.RegexTemplate
+import com.basgeekball.awesomevalidation.AwesomeValidation as Validation
 import fr.gobelins.edu.vandevelde.tp02_yoan_vandevelde.NavigationListener
 import fr.gobelins.edu.vandevelde.tp02_yoan_vandevelde.R
 import fr.gobelins.edu.vandevelde.tp02_yoan_vandevelde.data.NeighborRepository
@@ -30,7 +34,7 @@ class AddNeighbourFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         _binding = AddNeighborBinding.inflate(inflater, container, false)
-        return  binding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,25 +42,47 @@ class AddNeighbourFragment : Fragment() {
     }
 
     private fun submitButtonClick() {
-        binding.submitButton.setOnClickListener(View.OnClickListener {
+        binding.submitButton.setOnClickListener(
+                View.OnClickListener {
+                    validator()
 
+                    val newNeigbor = Neighbor(
+                            UUID.randomUUID().getMostSignificantBits(),
+                            binding.nameInput.text.toString(),
+                            binding.imageInput.text.toString(),
+                            binding.addressInput.text.toString(),
+                            binding.phoneInput.text.toString(),
+                            binding.aboutInput.text.toString(),
+                            false,
+                            binding.websiteInput.text.toString()
+                    )
 
-            val newNeigbor = Neighbor(
-                    UUID.randomUUID().getMostSignificantBits(),
-                    binding.nameInput.text.toString(),
-                    binding.imageInput.text.toString(),
-                    binding.addressInput.text.toString(),
-                    binding.phoneInput.text.toString(),
-                    binding.aboutInput.text.toString(),
-                    false,
-                    binding.websiteInput.text.toString()
-            )
-
-            NeighborRepository.getInstance().create(newNeigbor)
-            print("After created neighbor")
-
-
-        })
+                    NeighborRepository.getInstance().create(newNeigbor)
+                    print("After created neighbor")
+                }
+        )
     }
 
+    private fun validator() {
+        val formValidation = Validation(ValidationStyle.COLORATION)
+
+        formValidation.addValidation(activity, binding.nameInput.id, RegexTemplate.NOT_EMPTY, R.string.validation_message)
+
+        formValidation.addValidation(activity, binding.imageInput.id, RegexTemplate.NOT_EMPTY, R.string.validation_message)
+        formValidation.addValidation(activity, binding.imageInput.id, RegexTemplate.NOT_EMPTY, R.string.validation_web_url)
+
+        formValidation.addValidation(activity, binding.addressInput.id, RegexTemplate.NOT_EMPTY, R.string.validation_web_url)
+
+        formValidation.addValidation(activity, binding.phoneInput.id, RegexTemplate.NOT_EMPTY, R.string.validation_message)
+        formValidation.addValidation(activity, binding.phoneInput.id, android.util.Patterns.PHONE, R.string.validation_phone)
+
+        formValidation.addValidation(activity, binding.aboutInput.id, RegexTemplate.NOT_EMPTY, R.string.validation_web_url)
+        formValidation.addValidation(activity, binding.aboutInput.id, "/^[a-z]{0,30}\$/", R.string.validation_max_char)
+
+        formValidation.addValidation(activity, binding.websiteInput.id, android.util.Patterns.WEB_URL, R.string.validation_message)
+        formValidation.addValidation(activity, binding.websiteInput.id, RegexTemplate.NOT_EMPTY, R.string.validation_web_url)
+
+
+        formValidation.validate()
+    }
 }
